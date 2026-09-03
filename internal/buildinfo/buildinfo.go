8fb58b57ba822237926a1107eff28c813538ceaa
Starlight Unit Studios <pschildgen87@gmail.com>
2026-09-03 23:06:55 +0200

    Align Runtime build info with 0.2.0-alpha.2

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -2,7 +2,7 @@ package buildinfo
 
 // Values may be replaced with -ldflags during release builds.
 var (
-	Version   = "0.1.0-alpha.1"
+	Version   = "0.2.0-alpha.2"
 	Commit    = "development"
 	BuildDate = "unknown"
 )
