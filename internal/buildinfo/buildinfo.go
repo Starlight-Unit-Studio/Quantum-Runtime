b39742284c122dee647eaf8b5b1ed383e9a83a09
Starlight Unit Studios <pschildgen87@gmail.com>
2026-09-06 01:14:17 +0200

    Update build version for alpha.4

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -2,7 +2,7 @@ package buildinfo
 
 // Values may be replaced with -ldflags during release builds.
 var (
-	Version   = "0.3.0-alpha.3"
+	Version   = "0.3.0-alpha.4"
 	Commit    = "development"
 	BuildDate = "unknown"
 )
